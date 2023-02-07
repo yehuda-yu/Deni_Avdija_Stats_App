46f773d1ebb5bc31af5011b8c147c133f4d4af74
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-02-07 13:09:10 +0200

    Update Deni_Avdija_Stats.py

--- Deni_Avdija_Stats.py
+++ Deni_Avdija_Stats.py
@@ -23,7 +23,7 @@ script_directory = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(script_directory, 'favicon.png'), 'wb') as f:
     f.write(response.content)
 icon = Image.open(os.path.join(script_directory, 'favicon.png'))
-st.set_page_config(icon=icon, page_title="Deni Stats Explorer")
+st.set_page_config("Deni Stats Explorer",icon)
 
 @st.cache
 def get_career_df():
